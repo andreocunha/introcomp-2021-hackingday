264b3b74d72dec0402a726a7843a61351fd00001
andreocunha <andreoliveiracunha20@gmail.com>
2021-10-24 16:53:52 -0300

    adicionado estilos no card

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 function getUser() {
     let user = document.getElementById('username').value;
+
+    if(!user){
+        return null;
+    }
+
+    console.log(user);
     fetch('https://api.github.com/users/' + user)
     .then(function(response) {
         return response.json();
@@ -13,14 +19,31 @@ function getUser() {
                     <img src="${data.avatar_url}" alt="${data.login}">
                     <h2>${data.name}</h2>
                 </div>
-                <p>username: <b> ${data.login}</b></p>
-                <p>descrição: <b> ${data.bio}</b></p>
-                <p>endereço: <b> ${data.location}</b></p>
-                <p>repositórios públicos: <b> ${data.public_repos}</b></p>
-                <p>seguidores: <b> ${data.followers}</b></p>
-                <p>seguindo: <b> ${data.following}</b></p>
+                <div id="allinfo">
+                    <p>username: <b> ${data.login}</b></p>
+                    <p>descrição: <b> ${data.bio}</b></p>
+                    <p>endereço: <b> ${data.location}</b></p>
+                    
+                    <div id="numbersinfoarea">
+                        <div id="numbersinfo">
+                            <p><b>${data.public_repos}</b></p>
+                            <p>repositórios</p>
+                        </div>
+
+                        <div id="numbersinfo">
+                            <p><b>${data.followers}</b></p>
+                            <p>seguidores</p>
+                        </div>
+
+                        <div id="numbersinfo">
+                            <p><b>${data.following}</b></p>
+                            <p>seguindo</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         `;
+
     })
     .catch(function (error) {
         console.log("Error: " + error);
